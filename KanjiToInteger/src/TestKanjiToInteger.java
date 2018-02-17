4d5fc4ac4e21178fee8d8dba86a6d10811c22c90
yuyabu <yuyabushita@gmail.com>
2018-02-17 22:30:02 +0900

    init

---
+++ KanjiToInteger/src/TestKanjiToInteger.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class TestKanjiToInteger {
+	KanjiToInteger kti;
+	@Before
+	void init() {
+		kti = new KanjiToInteger();
+	}
+	@Test
+	void test() {
+		
+	}
+
+}
