423f91fe4111d365fb6e09177a9851e8920f75a4
yuyabu <yuyabushita@gmail.com>
2018-02-17 23:50:29 +0900

    変換できない漢字が来た時の例外を追加

--- KanjiToInteger/src/KanjiToInteger.java
+++ KanjiToInteger/src/KanjiToInteger.java
@@ -26,8 +26,9 @@ public class KanjiToInteger {
 				}
 			}
 			return strToInt(b.toString());
+		}else {
+			throw new NumberFormatException();
 		}
-		return null;
 	}
 	/**
 	 * 
--- KanjiToInteger/src/TestKanjiToInteger.java
+++ KanjiToInteger/src/TestKanjiToInteger.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
@@ -36,18 +37,19 @@ class TestKanjiToInteger {
 			Integer expected = 1984;
 			Integer number = kti.toInteger("一九八四");
 			assertEquals(expected,number);
-			
 		}
-		
-		
-		
+		@DisplayName("数値でない漢字が来た時の対応")
+		@Test
+		void 数値でない漢字が来た時の対応() {
+			try{
+				kti.toInteger("数値でない");
+				fail();
+			}catch(Exception e) {
+			}
+		}
 	}
 	@Nested
 	@DisplayName("そのほかのテスト")
 	class その他のテスト{
-		
 	}
-	
-	
-
 }
