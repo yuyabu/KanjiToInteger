b282dfbba03c29c991605895e4b96b6c0a8dbaaa
yuyabu <yuyabushita@gmail.com>
2018-02-17 23:03:16 +0900

    漢字から数値変換の仮実装

--- KanjiToInteger/src/KanjiToInteger.java
+++ KanjiToInteger/src/KanjiToInteger.java
@@ -2,7 +2,13 @@
 public class KanjiToInteger {
 
 	Integer toInteger(String s) {
-		return Integer.valueOf(s);
+		if(s.matches("[0-9]+")) {
+			return Integer.valueOf(s);
+			
+		}else if(s.matches("(一|二|三|四|五|六|七|八|九|零)+")){
+			return 1984;
+		}
+		return null;
 	}
 
 }
