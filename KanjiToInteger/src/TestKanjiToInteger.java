79034ff6df82eb7e9c5b62a3a7dc89ffe4fe9098
yuyabu <yuyabushita@gmail.com>
2018-02-17 22:52:20 +0900

    テストのリファクタリング
    入れこのテストケースに変更

--- KanjiToInteger/src/TestKanjiToInteger.java
+++ KanjiToInteger/src/TestKanjiToInteger.java
@@ -1,20 +1,39 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 class TestKanjiToInteger {
 	static KanjiToInteger kti;
 	@BeforeAll
 	static void init() {
 		kti = new KanjiToInteger();
 	}
-	@Test
-	void 文字型を数値型に変換() {
-		Integer expected = 123;
-		Integer number =  kti.toInteger("123");
-		assertEquals(expected,number);
+	@Nested
+	@DisplayName("数値のみ")
+	class 数値のみ{
+		@Test
+		void 文字型を数値型に変換() {
+			Integer expected = 123;
+			Integer number =  kti.toInteger("123");
+			assertEquals(expected,number);
+		}
+		
 	}
+	@Nested
+	@DisplayName("漢字が含まれる")
+	class 漢字が含まれる{
+		@Test
+		void 漢字を数値型に変更() {
+			
+		}
+	}
+	
 
 }
